9e4d0c5a6980d5c891be15d4c2e202e8b06fd05d
David <dluna777@outlook.com>
2024-09-24 10:03:34 -0700

    Initial

---
+++ decl.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TMin INT_MIN
+#define TMax INT_MAX
+
+#include "btest.h"
+#include "bits.h"
+
+test_rec test_set[] = {
+/* Copyright (C) 1991-2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses Unicode 10.0.0.  Version 10.0 of the Unicode Standard is
+   synchronized with ISO/IEC 10646:2017, fifth edition, plus
+   the following additions from Amendment 1 to the fifth edition:
+   - 56 emoji characters
+   - 285 hentaigana
+   - 3 additional Zanabazar Square characters */
+/* We do not support C11 <threads.h>.  */
+//1
+ {"bitAnd", (funct_t) bitAnd, (funct_t) test_bitAnd, 2, "| ~", 8, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"bitNor", (funct_t) bitNor, (funct_t) test_bitNor, 2, "& ~", 8, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isTmin", (funct_t) isTmin, (funct_t) test_isTmin, 1, "! ~ & ^ | +", 10, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"upperBits", (funct_t) upperBits, (funct_t) test_upperBits, 1, "! ~ & ^ | + << >>", 10, 1,
+  {{0, 32},{TMin,TMax},{TMin,TMax}}},
+ {"tmax", (funct_t) tmax, (funct_t) test_tmax, 0, "! ~ & ^ | + << >>", 4, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+//2
+ {"sign", (funct_t) sign, (funct_t) test_sign, 1, "! ~ & ^ | + << >>", 10, 1,
+     {{-TMax, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"floatNegate", (funct_t) floatNegate, (funct_t) test_floatNegate, 1,
+    "$", 10, 1,
+     {{1, 1},{1,1},{1,1}}},
+ {"allEvenBits", (funct_t) allEvenBits, (funct_t) test_allEvenBits, 1,
+    "! ~ & ^ | + << >>", 12, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"anyOddBit", (funct_t) anyOddBit, (funct_t) test_anyOddBit, 1,
+    "! ~ & ^ | + << >>", 12, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isPositive", (funct_t) isPositive, (funct_t) test_isPositive, 1,
+    "! ~ & ^ | + << >>", 8, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+{"byteSwap", (funct_t) byteSwap, (funct_t) test_byteSwap, 3,
+     "! ~ & ^ | + << >>", 25, 1,
+    {{TMin, TMax},{0,3},{0,3}}},
+ {"isNotEqual", (funct_t) isNotEqual, (funct_t) test_isNotEqual, 2,
+    "! ~ & ^ | + << >>", 6, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"copyLSB", (funct_t) copyLSB, (funct_t) test_copyLSB, 1,
+    "! ~ & ^ | + << >>", 5, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+//3
+ {"rotateLeft", (funct_t) rotateLeft, (funct_t) test_rotateLeft,
+   2, "! ~ & ^ | + << >>", 25, 1,
+  {{TMin, TMax},{0,31},{TMin,TMax}}},
+ {"logicalShift", (funct_t) logicalShift, (funct_t) test_logicalShift,
+   2, "! ~ & ^ | + << >>", 20, 1,
+  {{TMin, TMax},{0,31},{TMin,TMax}}},
+ {"isLess", (funct_t) isLess, (funct_t) test_isLess, 2,
+    "! ~ & ^ | + << >>", 24, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"multFiveEighths", (funct_t) multFiveEighths, (funct_t) test_multFiveEighths, 1,
+    "! ~ & ^ | + << >>", 12, 1,
+  {{-(1<<28)-1, (1<<28)-1},{TMin,TMax},{TMin,TMax}}},
+ {"subtractionOK", (funct_t) subtractionOK, (funct_t) test_subtractionOK, 2,
+    "! ~ & ^ | + << >>", 20, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+  {"", NULL, NULL, 0, "", 0, 0,
+   {{0, 0},{0,0},{0,0}}}
+};
